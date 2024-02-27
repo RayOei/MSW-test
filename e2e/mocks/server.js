38d58632f8190acbf74b0ac3a80963f5bc82d70c
Ray Oei <10501445+RayOei@users.noreply.github.com>
2024-02-27 12:46:04 +0100

    Add more event logging

--- e2e/mocks/server.js
+++ e2e/mocks/server.js
@@ -6,4 +6,22 @@ const server = setupServer(...handlers);
 server.events.on('request:start', ({request}) => {
   console.log('Outgoing:', request.method, request.url);
 });
+
+server.events.on('request:match', ({request}) => {
+  console.log('Matched:', request.method, request.url);
+});
+
+server.events.on('response:mocked', ({request, response}) => {
+  const payload = response.clone().text();
+  const cmd = payload._j;
+  console.log(
+    'Request: %s %s => Received %s %s => %s',
+    request.method,
+    request.url,
+    response.status,
+    response.statusText,
+    cmd,
+  );
+});
+
 export {server};
